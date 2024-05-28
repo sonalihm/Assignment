cb154cf7fde19437217dd29093108cf4bd1e4dc2
sonalihm <sonalim0219@gmail.com>
2024-05-28 15:28:30 +0530

    Create variable.tf

---
+++ Terraform/variable.tf
@@ -0,0 +1,9 @@
+variable "resource_group" {
+  description = "Name of Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Name of Location"
+  type        = string
+}
